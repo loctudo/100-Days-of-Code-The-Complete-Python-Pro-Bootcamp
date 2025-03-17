ef06367f97c1ccb9d9e25a59e4e726dde5b5133f
Huynh Tan Loc <igorhuynh117@gmail.com>
2025-03-17 15:36:34 +0700

    Add files via upload

---
+++ D14/main.py
@@ -0,0 +1,59 @@
+# Display art
+from art import logo, vs
+from game_data import data
+import random
+
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+
+# Make the game repeatable.
+while game_should_continue:
+    # Generate a random account from the game data.
+    # Making account at position B become next account at position A
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    def format_data(account):
+        # Takes the account data and return the printable format.
+        account_name = account["name"]
+        account_descr = account["description"]
+        account_country = account["country"]
+        return f"{account_name}, a {account_descr}, from {account_country}"
+
+    def check_answer(user_guess, a_followers, b_followers):
+        """Takes a user's guess ad the follower counts and returns if they got it right"""
+        if a_followers > b_followers:
+            return user_guess == "a"
+        else:
+            return user_guess == "b"
+
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}")
+
+    # Ask user for a guess.
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    # Clear the screen
+    print("\n" * 20)
+    print(logo)
+
+    # Check if user is correct.
+    ## Get follower count of each account.
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    ## Use if statement to check if user is correct.
+    # Give user feedback on their guess.
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score {score}")
+    else:
+        print(f"Sorry, that's wrong.Final score: {score}")
+        game_should_continue = False
